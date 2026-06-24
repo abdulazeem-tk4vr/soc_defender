69f3e591220b8be4dcef50ceb06879f104fb8262
Abdul Azeem <sabdulazeem01@gmail.com>
2026-06-24 16:23:47 +0300

    refactor: Update __init__.py to export new defender modules

--- defender/__init__.py
+++ defender/__init__.py
@@ -1,6 +1,18 @@
 """soc_defender core package."""
 
 from .agent import SocDefenderAgent, build_agent
+from .graph import DefenderGraph
+from .graph_state import DefenderGraphState
 from .policy import DefenderPolicy
+from .regex_classifier import RegexPromptInjectionClassifier
+from .scanner import InjectionScanner
 
-__all__ = ["DefenderPolicy", "SocDefenderAgent", "build_agent"]
+__all__ = [
+    "DefenderPolicy",
+    "DefenderGraph",
+    "DefenderGraphState",
+    "InjectionScanner",
+    "RegexPromptInjectionClassifier",
+    "SocDefenderAgent",
+    "build_agent",
+]
